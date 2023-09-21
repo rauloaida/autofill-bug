7bdde7ce8e7a76dc01fbad73c12914b8fb75d600
Raul <raul.oaida.m@gmail.com>
2023-09-21 13:44:51 -0400

    first commit

---
+++ reshaped.config.js
@@ -0,0 +1,15 @@
+/**
+ * Configuration Documentation: https://reshaped.so/content/docs/theming/creating-themes
+ */
+/** @type {import('reshaped').ReshapedConfig} */
+const config = {
+	themes: {
+	  productTheme: {
+		color: {
+		  foregroundNeutral: { hex: "#1a1a1a", hexDark: "#fff" },
+		},
+	  },
+	},
+  };
+  
+  module.exports = config;
---
+++ src/app/page.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import React, { useState } from 'react';
+import { View, Reshaped, TextField, Text} from 'reshaped';
+import 'reshaped/themes/reshaped/theme.css';
+
+export default function Home() {
+  const [email, setEmail] = useState<string>("");
+
+  const handleChange = ({ value }: { value: string; name: string; event?: React.ChangeEvent<HTMLInputElement> }) => {
+    setEmail(value);
+  };
+
+  return (
+    <Reshaped theme="reshaped">
+        <View padding={12} direction='column' align='center' gap={8}>
+          <Text variant='featured-2'>Autofill input field with a value saved in your browser to see error</Text>
+            <TextField
+              size='large'
+              name="email"
+              placeholder="your.email@gmail.com"
+              value={email}
+              onChange={handleChange}
+            />
+        </View>
+    </Reshaped>
+  );
+}
